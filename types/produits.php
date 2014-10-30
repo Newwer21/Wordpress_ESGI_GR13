6c7d37f8fc8c4e4b7431e07c954abc0b189edb41
philippe <philippesousa93@gmail.com>
2014-10-30 22:57:36 +0100

    admin- meta_box
    personnalisation de produit

--- types/produits.php
+++ types/produits.php
@@ -8,7 +8,7 @@
  * @Author: Fy
  * @Date:   2014-10-28 23:23:34
  * @Last Modified by:   Fy
- * @Last Modified time: 2014-10-29 17:56:26
+ * @Last Modified time: 2014-10-30 22:56:58
  */
 
 add_action( 'init', 'produits_register' );
@@ -44,7 +44,24 @@ function produits_register() {
 	  );
  
 	register_post_type( 'produits' , $args );
-
 	register_taxonomy("Types", array("produits"), array("hierarchical" => true, "label" => "Types", "singular_label" => "type", "rewrite" => true));
 
+/* META_BOX PRODUITS */
+	add_action("admin_init", "newProduit_init");
+	function newProduit_init(){
+		  add_meta_box("caract_id", "caracteristique", "caract_produit", "produits", "normal", "low");
+	/*	  add_meta_box("credits_meta", "Design &amp; Build Credits", "credits_meta", "portfolio", "normal", "low");
+	*/	}
+
+	function caract_produit(){
+	  global $post;
+	  $custom = get_post_custom($post->ID);
+	  $year_completed = $custom["prix_prod"][0];
+
+	   echo 'box prix';  
+	   echo 'box taille ecran';  
+	   echo 'box processeur';  
+
+	}
+/* META_BOX PRODUITS */
 }
