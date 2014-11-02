60d829af583a78586c83c287c2771d4db3ce3c53
philippe <philippesousa93@gmail.com>
2014-11-02 18:35:24 +0100

    admin - suppr article

--- functions.php
+++ functions.php
@@ -49,4 +49,20 @@ include_once plugin_dir_path( __FILE__ ).'/types/produits.php';
 	}
 	
 	add_filter('widget_text','php_execute',100);
+
+	// surprime Article et page
+
+
+function remove_menu_items() {
+  global $menu;
+  $restricted = array(__('Posts'));
+  end ($menu);
+  while (prev($menu)){
+    $value = explode(' ',$menu[key($menu)][0]);
+    if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){
+      unset($menu[key($menu)]);}
+    }
+  }
+
+add_action('admin_menu', 'remove_menu_items');
 ?>
