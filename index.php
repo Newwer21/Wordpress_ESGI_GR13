1e85c881f6304a1da602d5fae990226eee4ff301
Newwer21 <j-jac_ly@hotmail.fr>
2014-11-02 17:16:37 +0100

    produits.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	echo 'Index Page';
-	get_header();
-	get_sidebar();
-	get_template_part('loop');
-	
-	get_footer(); 
-?>
--- sidebar.php
+++
@@ -1,3 +0,0 @@
-<aside class="sidebar-1"><?php dynamic_sidebar('zone_widget_gauche'); ?>
-	<div class="fix-clear"></div>
-</aside>
